7953a6e686ea0b1e76ebc02988f2428984a29b2c
Johnson <kaguithiajohnson@gmail.com>
2019-07-27 21:51:52 +0300

    Updated shoppinglist app with continuos deployment to pypi from circleci

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 
-VERSION = "0.1"
+VERSION = "1.0"
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
