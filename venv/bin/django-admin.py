28a79ff9ec70b53d9f3767024a8d350f52e7b012
Johnson <kaguithiajohnson@gmail.com>
2019-07-31 08:43:57 +0300

    Ignore virtualenv while pushing to master

--- venv/bin/django-admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/home/johnson/Desktop/Django/AppRepo/venv/bin/python3
-=======
-#!/home/johnson/AppRepo/venv/bin/python3
->>>>>>> 3bcba8ca754f5b048bfdf58cb58e955b402b8735
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
